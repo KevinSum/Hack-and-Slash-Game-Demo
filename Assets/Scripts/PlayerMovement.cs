cc4ff6f7fa6188e8ce1538f821def0c0cf942865
Kevin Sum <kevinsum20051996@hotmail.com>
2020-08-28 00:39:29 +0100

    Running and standing animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    private Rigidbody2D playerRigidBody;
+    public bool facingRight;
     private Vector2 movementInput;
+    private Rigidbody2D playerRigidBody;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,7 +29,12 @@ public class PlayerMovement : MonoBehaviour
     {
         if (movementInput != Vector2.zero)
         {
-            MoveCharacter();
+            animator.SetBool("running", true);
+            moveCharacter();
+        }
+        else
+        {
+            animator.SetBool("running", false);
         }
     }
     Vector2 getMovementInput()
@@ -33,9 +42,23 @@ public class PlayerMovement : MonoBehaviour
         return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
-    void MoveCharacter()
+    void moveCharacter()
     {
+        // Move Player
         Vector2 currentpos = new Vector2(transform.position.x, transform.position.y);
         playerRigidBody.MovePosition(currentpos + movementInput.normalized * speed * Time.deltaTime);
+        spriteFlipCheck();
+        // Set animator parameters. The animator will play animation based on these (as well as the 'running' bool parameter)
+        animator.SetFloat("moveX", movementInput.x);
+        animator.SetFloat("moveY", movementInput.y);
+    }
+
+    void spriteFlipCheck() // Flip sprite if needed
+    {
+        if(movementInput.x > 0 && facingRight == false || movementInput.x < 0 && facingRight == true)
+        {
+            facingRight = !facingRight;
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
     }
 }
