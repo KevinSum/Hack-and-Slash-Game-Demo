0638714c43c6e0e29c633b89827e4e36fb761e3f
Kevin Sum <kevinsum20051996@hotmail.com>
2020-09-04 01:15:32 +0100

    Camera Bounding. Used negative tile gap to fix tile tearing.

--- Assets/Scenes/CameraMovement.cs
+++ Assets/Scenes/CameraMovement.cs
@@ -6,6 +6,8 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform playerTransform;
     public float cameraSmoothing;
+    public Vector2 maxCameraPosition;
+    public Vector2 minCameraPosition;
     void Start()
     {
         
@@ -19,6 +21,9 @@ public class CameraMovement : MonoBehaviour
         {
             // Find camera target. Z position needs to stay consistant (-10), and is not the same as player position X.
             Vector3 cameraTarget = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+            // Bound camera
+            cameraTarget.x = Mathf.Clamp(cameraTarget.x, minCameraPosition.x, maxCameraPosition.x);
+            cameraTarget.y = Mathf.Clamp(cameraTarget.y, minCameraPosition.y, maxCameraPosition.y);
             // Move camera towards target (which should be set to player position)
             transform.position = Vector3.Lerp(transform.position, cameraTarget, cameraSmoothing);
         }
