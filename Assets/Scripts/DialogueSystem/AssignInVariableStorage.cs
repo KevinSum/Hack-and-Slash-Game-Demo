0344c2196e9e6d1c18d9fe36f101c6ff32ab9194
kevinsum <Kevinsum20051996@hotmail.com>
2020-11-11 01:06:35 +0000

    Move InMemoryVariableStorage component to player, since player doesn't get destroyed during scene transition.

---
+++ Assets/Scripts/DialogueSystem/AssignInVariableStorage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+// Assign Variable Storage contained in Player gameObject (which should not be destroyed during scene transition) 
+// for Yarn Dialogue Runner through script, just so that I don't have to do it in inspector.
+public class AssignInVariableStorage : MonoBehaviour
+{
+    DialogueRunner dialogueRunner;
+    GameObject player;
+
+    void Awake()
+    {
+        dialogueRunner = GetComponent<DialogueRunner>();
+        player = GameObject.Find("Player");
+        dialogueRunner.variableStorage = player.GetComponent<InMemoryVariableStorage>();
+    }
+
+}
