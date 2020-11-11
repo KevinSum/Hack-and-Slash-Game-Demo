f6f541c573665506f2fbdcea1243a4090924fa00
kevinsum <Kevinsum20051996@hotmail.com>
2020-11-11 01:12:07 +0000

    Scene Transition code clean up

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -7,19 +7,16 @@ public class SceneTransition : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
     [SerializeField] private Vector2 newPlayerPosition;
-    [SerializeField] private SetPlayerTransitionParams playerTransitionParams;
     private GameObject player;
 
     private void Start()
     {
-        playerTransitionParams = GameObject.Find("Player").GetComponent<SetPlayerTransitionParams>();
         player = GameObject.Find("Player");
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") && !collision.isTrigger)
         {
-            playerTransitionParams.setInitialPosition(newPlayerPosition);
             player.transform.position = newPlayerPosition;
             SceneManager.LoadScene(sceneToLoad);
 
