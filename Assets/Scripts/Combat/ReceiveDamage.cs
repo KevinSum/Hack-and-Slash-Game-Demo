0396f94f69c5e0a8152a0dba28a5939dd7461fd0
kevinsum <Kevinsum20051996@hotmail.com>
2020-11-17 02:36:08 +0000

    Knockback animation

--- Assets/Scripts/Combat/ReceiveDamage.cs
+++ Assets/Scripts/Combat/ReceiveDamage.cs
@@ -13,7 +13,7 @@ public class ReceiveDamage : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    public void ReceiveAttackSignal(string tag, float damage, float thrust, Vector3 enemy)
+    public void ReceiveAttackSignal(string tag, float damage, float thrust, Vector3 enemyPos)
     {
         // Check if tag from the originated attack signal is from an entity that can deal damage to this entity.
         for (int i = 0; i < damageSources.Length; i++)
@@ -23,21 +23,30 @@ public class ReceiveDamage : MonoBehaviour
                 if (!animator.GetBool("invincible"))
                 {
                     animator.SetFloat("health", animator.GetFloat("health") - damage);
-                    StartCoroutine(KnockCoroutine(enemy, thrust));
+
+                    StartCoroutine(KnockbackCoroutine(enemyPos, thrust));
                 }
             }
         }
         
     }
 
-    private IEnumerator KnockCoroutine(Vector3 enemyPosition, float thrust)
+    private IEnumerator KnockbackCoroutine(Vector3 enemyPos, float thrust)
     {
-        Vector2 forceDirection = transform.position - enemyPosition;
-        Vector2 force = forceDirection.normalized * thrust;
+        animator.SetTrigger("staggered");
+        animator.SetBool("canMove", false);
 
+        // Apply force knockback
+        Vector2 forceDirection = transform.position - enemyPos;
+        Vector2 force = forceDirection.normalized * thrust;
         rigidbody.velocity = force;
-        yield return new WaitForSeconds(.3f);
+
+        yield return new WaitForSeconds(0.001f); // wait one frame for animator to switch to staggered animation. Then we can get the length of the animation;
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorClipInfo(0)[0].clip.length - 0.001f);
 
         rigidbody.velocity = new Vector2();
+
+        animator.SetBool("canMove", true);
     }
+
 }
